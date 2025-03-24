4970ea2ec01416d5dd8b4f1b5af241e0d2ce31ae
Eduardo de Andrade do Bomfim Júnior <eduardo.a.junior.dev@gmail.com>
2025-03-24 14:13:56 -0300

    projeto finalizado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Agência!");
+        String agencia = sc.next();
+
+        System.out.println("Por favor, digite o número da sua conta!");
+        int numero = sc.nextInt();
+
+        System.out.println("Por favor, digite o seu nome!");
+        String nomeCliente = sc.next();
+
+        System.out.println("Por favor, digite o saldo da sua conta!");
+        float saldo = sc.nextFloat();
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque", nomeCliente, agencia, numero, saldo);
     }
 }
