9b237deda4268daf6c0b8da3dbb85fbd2aa68fa1
Eduardo de Andrade do Bomfim Júnior <eduardo.a.junior.dev@gmail.com>
2025-03-26 19:52:26 -0300

    :sparkles: feat: Integrando as classes no Iphone.

--- DesafioUML/src/Iphone.java
+++ DesafioUML/src/Iphone.java
@@ -1,5 +1,23 @@
 public class Iphone {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+    
+        ReprodutorMusical reprodutorMusical = new ReprodutorMusical();
+        AparelhoTelefonico aparelhoTelefonico = new AparelhoTelefonico();
+        NavegadorInternet navegadorInternet = new NavegadorInternet();
+
+        System.out.println("\nTestando o Reprodutor Musical:");
+        reprodutorMusical.selecionarMusica("Imagine - John Lennon");
+        reprodutorMusical.tocar();
+        reprodutorMusical.pausar();
+
+        System.out.println("\nTestando o Aparelho Telefônico:");
+        System.out.println(aparelhoTelefonico.ligar("11999999999"));
+        aparelhoTelefonico.atender();
+        aparelhoTelefonico.iniciarCorreioVoz();
+
+        System.out.println("\nTestando o Navegador de Internet:");
+        navegadorInternet.exibirPagina("www.google.com");
+        navegadorInternet.adicionarNovaAba();
+        navegadorInternet.atualizarPagina();
     }
 }
