7aac75234ee53c0f4bb919549a7d37fb5c0cbbcf
Eduardo de Andrade do Bomfim Júnior <eduardo.a.junior.dev@gmail.com>
2025-04-11 22:50:12 -0300

    :sparkles: feat: Adicionando a classe NumberText como textField

--- DesafioSudoku/src/main/java/org/example/ui/custom/panel/SudokuSector.java
+++ DesafioSudoku/src/main/java/org/example/ui/custom/panel/SudokuSector.java
@@ -1,12 +1,15 @@
 package org.example.ui.custom.panel;
 
+import org.example.ui.custom.input.NumberText;
+
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.util.List;
 
 public class SudokuSector extends JPanel {
 
-    public SudokuSector() {
+    public SudokuSector(final List<NumberText> numberTexts) {
         var dimensions = new Dimension(170, 170);
         this.setSize(dimensions);
         this.setPreferredSize(dimensions);
@@ -15,5 +18,6 @@ public class SudokuSector extends JPanel {
         this.setLayout(new GridLayout(3, 3));
         this.setBorder(new LineBorder(Color.BLACK, 2, true));
         this.setVisible(true);
+        numberTexts.forEach(this::add);
     }
 }
